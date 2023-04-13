6de1709fa8b6c8595379d0a985745a0bfb8ad7e2
Djcharles26 <jlaraanaya@gmail.com>
2023-04-13 02:10:14 -0600

    Updated bottom bar with orientation builder

--- lib/widgets/common/bottom_bar.dart
+++ lib/widgets/common/bottom_bar.dart
@@ -38,89 +38,97 @@ class _BottomBarState extends State<BottomBar> {
 
   @override
   Widget build(BuildContext context) {
-    return Container (
-    margin: EdgeInsets.only (
-      bottom: Platform.isIOS 
-      ? 24
-      : kBottomNavigationBarHeight,
-      left: 8,
-      right: 8
-    ),
-    padding: const EdgeInsets.symmetric(
-      vertical: 8 
-    ),
-    decoration: BoxDecoration (
-      color: Theme.of(context).colorScheme.background,
-      borderRadius: BorderRadius.circular(72)
-    ),
-    child: Row (
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        _button (
-          icon: Icons.house_outlined,
-          selectedIcon: Icons.house_sharp,
-          route: "/home"
+    return OrientationBuilder(
+      builder: (context, orientation) {
+        return Container (
+        margin: EdgeInsets.only (
+          bottom: orientation == Orientation.landscape
+          ? 16
+          : Platform.isIOS 
+          ? 24
+          : kBottomNavigationBarHeight,
+          left: orientation == Orientation.landscape
+          ? MediaQuery.of(context).size.width * 0.3 : 8,
+          right: orientation == Orientation.landscape
+          ? MediaQuery.of(context).size.width * 0.3 : 8,
         ),
-        _button (
-          icon: Icons.report_problem_outlined,
-          selectedIcon: Icons.report_problem_sharp,
-          route: "/reports"
+        padding: const EdgeInsets.symmetric(
+          vertical: 8 
         ),
-        SizedBox.square(
-          dimension: 32,
-          child: Stack(
-            children: [
-              Positioned.fill(
-                child: _button (
-                  icon: Icons.notifications_outlined,
-                  selectedIcon: Icons.notifications_sharp,
-                  route: "/notifications"
-                ),
-              ),
-              Positioned (
-                top: 0,
-                right: 0,
-                child: StreamBuilder(
-                  stream: NotificationsManager.instance.notificationStream,
-                  builder: (context, _) {
-                    return Visibility(
-                      visible: NotificationsManager
-                        .instance.getUnreadNotificationsLength () > 0,
-                      child: Container (
-                        width: 16,
-                        height: 16,
-                        padding: const EdgeInsets.all(2),
-                        decoration: const BoxDecoration (
-                          color: UIColors.error,
-                          shape: BoxShape.circle
-                        ),
-                        child: FittedBox(
-                          fit: BoxFit.contain,
-                          child: Text (
-                            "${
-                              NotificationsManager.instance.getUnreadNotificationsLength ()
-                            }",
-                            style: Theme.of(context).textTheme.bodySmall!.copyWith(
-                              color: UIColors.white
+        decoration: BoxDecoration (
+          color: Theme.of(context).colorScheme.background,
+          borderRadius: BorderRadius.circular(72)
+        ),
+        child: Row (
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            _button (
+              icon: Icons.house_outlined,
+              selectedIcon: Icons.house_sharp,
+              route: "/home"
+            ),
+            _button (
+              icon: Icons.report_problem_outlined,
+              selectedIcon: Icons.report_problem_sharp,
+              route: "/reports"
+            ),
+            SizedBox.square(
+              dimension: 32,
+              child: Stack(
+                children: [
+                  Positioned.fill(
+                    child: _button (
+                      icon: Icons.notifications_outlined,
+                      selectedIcon: Icons.notifications_sharp,
+                      route: "/notifications"
+                    ),
+                  ),
+                  Positioned (
+                    top: 0,
+                    right: 0,
+                    child: StreamBuilder(
+                      stream: NotificationsManager.instance.notificationStream,
+                      builder: (context, _) {
+                        return Visibility(
+                          visible: NotificationsManager
+                            .instance.getUnreadNotificationsLength () > 0,
+                          child: Container (
+                            width: 16,
+                            height: 16,
+                            padding: const EdgeInsets.all(2),
+                            decoration: const BoxDecoration (
+                              color: UIColors.error,
+                              shape: BoxShape.circle
+                            ),
+                            child: FittedBox(
+                              fit: BoxFit.contain,
+                              child: Text (
+                                "${
+                                  NotificationsManager.instance.getUnreadNotificationsLength ()
+                                }",
+                                style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                                  color: UIColors.white
+                                ),
+                              ),
                             ),
                           ),
-                        ),
-                      ),
-                    );
-                  }
-                ),
-              )
-            ],
-          ),
+                        );
+                      }
+                    ),
+                  )
+                ],
+              ),
+            ),
+            _button (
+              icon: Icons.person_outline,
+              selectedIcon: Icons.person_sharp,
+              route: "/account"
+            )
+          ],
         ),
-        _button (
-          icon: Icons.person_outline,
-          selectedIcon: Icons.person_sharp,
-          route: "/account"
-        )
-      ],
-    ),
   );
+      }
+    );
   }
 }
